7c483275de97b074dd63fd6cf489e1c592994534
kbl <m.pietraszek@gmail.com>
2011-12-28 11:15:14 +0100

    gets harder (;

--- challenge_07.py
+++ challenge_07.py
@@ -1,9 +1,18 @@
-import Image
+import Image, re
 
 img = Image.open('challenge_07.png')
 band = img.crop((0, 43, 608, 44))
 
 letters = []
 
-for letter in list(band.getdata()):
-    print chr(letter[0])
+for x in range(0, 608, 7):
+        (r, g, b, a) = band.getpixel((x, 0))
+        letters.append(chr(r))
+
+sentence = ''.join(letters)
+print sentence 
+
+numbers = re.search('^.+\[(.+)\]$', sentence).group(1).split(',')
+letters = [chr(int(n.strip())) for n in numbers]
+sentence = ''.join(letters)
+print sentence
