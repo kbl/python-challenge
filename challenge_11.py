ec64d6e6d88a24f6d30eb4a286465f6cb155bcdd
kbl <m.pietraszek@gmail.com>
2012-01-19 20:09:42 +0100

    splitting in CMYK mode also ends with failure

--- challenge_11.py
+++ challenge_11.py
@@ -1,10 +1,12 @@
 import Image
 
 picture = Image.open('challenge_11.jpg')
+x = picture.convert('CMYK')
 
-picture.load()
-result1, result2, result3 = picture.split()
+x.load()
+result1, result2, result3, result4 = x.split()
 
 result1.save('challenge_11_result_1.jpg')
 result2.save('challenge_11_result_2.jpg')
 result3.save('challenge_11_result_3.jpg')
+result4.save('challenge_11_result_4.jpg')
