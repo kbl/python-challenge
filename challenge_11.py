1a255abe9c81ecbac8da0a0e5254e581f238a30e
kbl <m.pietraszek@gmail.com>
2012-01-19 20:02:27 +0100

    shorter way of previous one

--- challenge_11.py
+++ challenge_11.py
@@ -1,18 +1,9 @@
 import Image
 
-image = Image.open('challenge_11.jpg')
-length, width = image.size
+picture = Image.open('challenge_11.jpg')
 
-result1 = Image.new('L', (length, width))
-result2 = Image.new('L', (length, width))
-result3 = Image.new('L', (length, width))
-
-for x in xrange(length):
-    for y in xrange(width):
-        a, b, c = image.getpixel((x, y))
-        result1.putpixel((x, y), a)
-        result2.putpixel((x, y), b)
-        result3.putpixel((x, y), c)
+picture.load()
+result1, result2, result3 = picture.split()
 
 result1.save('challenge_11_result_1.jpg')
 result2.save('challenge_11_result_2.jpg')
