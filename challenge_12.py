e6fba6ef8f57b637471f1df22a5ea52be66faa2f
kbl <m.pietraszek@gmail.com>
2012-01-31 10:37:39 +0100

    closer with each step (;

--- challenge_12.py
+++ challenge_12.py
@@ -5,25 +5,8 @@ length, height = picture.size
 
 new_image = []
 
-def save_image(data, name):
-    # image = Image.new('RGB', picture.size)
-    # image.putdata(data)
-    image = Image.fromstring('RGB', (640, 480), data)
-    image.save(name)
-
-
-for x in xrange(length):
-    for y in xrange(height):
-        pixel = picture.getpixel((x, y))
-        try:
-            row = new_image[y]
-        except IndexError:
-            row = []
-            new_image.append(row)
-        row.append(pixel)
-
-print len(new_image)
-print len(new_image[0])
-
-save_image(new_image, 'test.jpg')
+modified = picture.transform(picture.size, 
+                             Image.AFFINE, 
+                             (1, 0, 0, 0, 1, -3))
+modified.show()
 
